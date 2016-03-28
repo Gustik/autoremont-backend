449a2c8f2ed280640828b338764ca0501e458f87
Владислав Тетерин <teterinlv@jamba.bbtech.local>
2016-03-28 12:36:54 +0900

    Стилистические изменения

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,19 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>Авторемонт</h1>
 
-        <p class="lead">Новое эпоха в сфере ремонта Вашего автомобиля</p>
+        <p class="lead">Новая эпоха в сфере ремонта Вашего автомобиля</p>
 
         <p>
-	        <a class="btn btn-lg btn-success" href="https://play.google.com/store/apps/details?id=pro.bbgroup.megamech.full" target="_blank">Скачать в Google Play</a>
-	        <a class="btn btn-lg btn-info" href="https://itunes.apple.com/us/app/avtoremont/id1025594490?l=ru&ls=1&mt=8" target="_blank">Скачать в AppStore</a>
+        	<div class="col-sm-6 hidden-xs">
+	        	<a class="btn btn-lg btn-block btn-success" href="https://play.google.com/store/apps/details?id=pro.bbgroup.megamech.full" target="_blank">Скачать в Google Play</a>
+        	</div>
+        	<div class="col-sm-6 hidden-xs">
+	        	<a class="btn btn-lg btn-block btn-info" href="https://itunes.apple.com/us/app/avtoremont/id1025594490?l=ru&ls=1&mt=8" target="_blank">Скачать в AppStore</a>
+        	</div>
+        	<div class="visible-xs">
+	        	<a class="btn btn-lg btn-block btn-success" href="https://play.google.com/store/apps/details?id=pro.bbgroup.megamech.full" target="_blank">Скачать в Google Play</a>
+	        	<a class="btn btn-lg btn-block btn-info" href="https://itunes.apple.com/us/app/avtoremont/id1025594490?l=ru&ls=1&mt=8" target="_blank">Скачать в AppStore</a>
+        	</div>
         </p>
     </div>
 </div>

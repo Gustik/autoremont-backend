b42680cedb368d3ae2b038764d19b448e9d513df
Густав Кутугутов <gustav.kutugutov@gmail.com>
2016-10-16 21:52:29 +0900

    account proceed
    Добавил консольный контроллер, который декрементирует дни в аккаунтинге.
    Его нужно добавить в крон на ежесуточное выполнение

---
+++ commands/BillAccountController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\commands;
+
+use Yii;
+use yii\console\Controller;
+
+class BillAccountController extends Controller
+{
+    public function actionProceed()
+    {
+        $sql = "
+            SET @diff := 0;
+            UPDATE bill_account AS ba, user AS u
+            SET
+                ba.processed_at = IF((@diff:=DATEDIFF(NOW(), processed_at)) > 0, NOW(), ba.processed_at),
+                ba.days = IF(ba.days <= 0, 0, ba.days - @diff),
+                u.can_work = IF(ba.days <= 0, false, true)
+            WHERE
+                ba.user_id = u.id
+         ";
+        $connection = Yii::$app->getDb();
+        $command = $connection->createCommand($sql);
+
+        return $command->execute();
+    }
+}

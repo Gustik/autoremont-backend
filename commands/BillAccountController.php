a251dd5810291062e8826fbbfe50b364e05c9365
Густав Кутугутов <gustav.kutugutov@gmail.com>
2017-05-22 16:39:10 +0900

    fix account calculating

--- commands/BillAccountController.php
+++ commands/BillAccountController.php
@@ -3,8 +3,13 @@
 namespace app\commands;
 
 use app\models\BillAccount;
+use DateTime;
 use Yii;
 use yii\console\Controller;
+use yii\console\Exception;
+use yii\db\Query;
+use yii\web\NotAcceptableHttpException;
+
 
 class BillAccountController extends Controller
 {
@@ -13,21 +18,60 @@ class BillAccountController extends Controller
         return static::decrementDay();
     }
 
+    /**
+     * @param $startDate string YYYY-MM-DD H:i:s Format
+     * @param $endDate string YYYY-MM-DD H:i:s Format
+     * @return integer count of diff days
+     */
+    public static function diffDays($startDate, $endDate)
+    {
+        $datetime1 = new DateTime($startDate);
+        $datetime2 = new DateTime($endDate);
+        $interval = $datetime1->diff($datetime2, false);
+
+        return intval($interval->format('%d'));
+    }
+
+    /**
+     * @throws NotAcceptableHttpException
+     * @throws \yii\db\Exception
+     */
     public static function decrementDay()
     {
-        $sql = '
-            SET @diff := 0, @day = 1440;
-            UPDATE bill_account AS ba, user AS u
-            SET
-                ba.processed_at = IF((@diff:=TIMESTAMPDIFF(MINUTE, ba.processed_at, NOW())) >= @day, NOW(), ba.processed_at),
-                ba.days = IF(ba.days <= 0, 0, IF(@diff >= @day, ba.days - (@diff div @day), ba.days)),
-                u.can_work = IF(ba.days <= 0, false, true)
-            WHERE
-                ba.user_id = u.id
-         ';
-        $connection = Yii::$app->getDb();
-        $command = $connection->createCommand($sql);
+        /**
+         * @var $accounts BillAccount[]
+         */
+        $accounts = BillAccount::find()->all();
+        $now = (new Query())->select('NOW() as d')->one()['d'];
+        $connection = \Yii::$app->db;
+        $transaction = $connection->beginTransaction();
+        try {
+            foreach($accounts as $account) {
 
-        return $command->execute();
+                $diff = static::diffDays($account->processed_at, $now);
+                $resDays = $account->days - $diff;
+                if($resDays <= 0) {
+                    $account->days = 0;
+                    $account->user->can_work = 0;
+                    $account->processed_at = $now;
+                } else {
+                    $account->days = $resDays;
+                    $account->user->can_work = 1;
+                    $account->processed_at = $now;
+                }
+
+                if (!$account->user->save()) {
+                    throw new Exception('Ошибка сохранения пользователя');
+                }
+                if (!$account->save()) {
+                    throw new Exception('Ошибка сохранения аккаунтинга');
+                }
+
+            }
+            $transaction->commit();
+        } catch (Exception $e) {
+            $transaction->rollback();
+            throw new Exception($e->getMessage());
+        }
     }
 }
